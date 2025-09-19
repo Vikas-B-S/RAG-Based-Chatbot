f81855f97332a42571d014fb91181b49540fa92f
Vikas-B-S <reachvikasbs@gmail.com>
2025-09-19 18:56:51 +0530

    Update pdfextracter.py

--- pdfextracter.py
+++ pdfextracter.py
@@ -1,10 +1,22 @@
-from pypdf import PdfReader
 
-def text_extractor_pdf(file_path):
-    pdf_file = PdfReader(file_path)
-    pdf_text = ''
-    for pages in pdf_file.pages:
-        text_only= pages.extract_text()
-        if text_only:
-          pdf_text += text_only
-    return pdf_text
+from pypdf import PdfReader
+import io
+
+def extract_text_from_pdf(file):
+        # Ensure a seekable BytesIO stream
+    text = ""
+    try:
+        file.seek(0)
+        pdf_stream = io.BytesIO(file.read())
+
+        pdf_reader = PdfReader(pdf_stream)
+
+        for page in pdf_reader.pages:
+            page_text = page.extract_text()
+            if page_text:
+                text += page_text + "\n"
+
+    except Exception as e:
+        text = f"Error reading PDF: {e}"
+
+    return text
